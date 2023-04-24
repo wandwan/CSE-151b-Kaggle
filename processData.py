0ede52aeb1b5cd73abc30da5503ce4bbae6ffef9
Andrew Wang <wangandrewwang88@gmail.com>
2023-04-24 13:28:52 -0700

    fixed everything

--- processData.py
+++ processData.py
@@ -1,70 +1,36 @@
+from functools import reduce
 import pandas as pd
 import numpy as np
 import math
 import ast
 
-# Define a function that converts a string to a numpy array
-def string_to_array(s):
-    # Use ast.literal_eval to evaluate the string as a list of lists
-    lst = ast.literal_eval(s)
-    # Use np.array to convert the list of lists to a numpy array
-    arr = np.array(lst)
-    # Return the numpy array
-    return arr
-
-def split_list(lst):
-  print(lst)
-  lst = [(math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2),
-          math.atan2(y[1] - x[1], y[0] - x[0])) for x, y in lst]
-  lst.insert(0, (0, 0))
-  print(lst)
-  ans = []
-  # Initialize an empty list to store the sequences
-  seqs = []
-  # If the length of lst is less than 8, pad with [0,0]
-  if len(lst) < 8:
-    seq = list(lst)
-    while len(seq) < 8:
-      seq.append(np.array([0, 0]))
-    seq = np.array(seq)
-    seqs.append(seq)
-    ans.append(np.array([0,0]))
-  else:
-    # Loop through the list with a step size of 4
-    for i in range(0, len(lst) - 7, 4):
-      # Slice the list from i to i + 8 and append it to the seqs list
-      seqs.append(lst[i:i + 8])
-      if(i + 8 >= len(lst)):
-        ans.append(np.array([0,0]))
-      else:
-        ans.append(lst[i + 8])
-  return seqs, ans
-
-
 # Define the chunksize
 chunksize = 1000
+ORIG_COORD = [-8.60789730,41.14766290]
 
 # Read the data in chunks
 for i, chunk in enumerate(pd.read_csv('train.csv', chunksize=chunksize)):
-    # Filter out missing data and select the POLYLINE column
     df = chunk.loc[chunk['MISSING_DATA'] == False, 'POLYLINE']
     # Split each row of the POLYLINE column into overlapping sequences of length 8
-    arr = None
-    out = None
+    result, ans = [], []
     for row in df:
-      row = string_to_array(row)
-      if len(row) == 0:
+      row = [ORIG_COORD, *ast.literal_eval(row)]
+      if len(row) <= 2:
         continue
-      result, ans = [np.stack(x, axis=0) for x in split_list(row)]
-      if arr is not None:
-        arr = np.append(arr, result, axis=0)
-      else:
-        arr = result
-      if out is not None:
-        out = np.append(out, ans, axis=0)
-      else:
-        out = ans
-    
+      if len(row) < 9:
+        row = [*row, *([row[-1]] * (9 - len(row)))]
+      for i in range(len(row) - 1, 0, -1):
+        x,x1 = row[i - 1], row[i]
+        row[i] = (math.atan2(x1[1] - x[1], x1[0] - x[0]), 
+                  math.sqrt((x1[1] - x[1])**2 + (x1[0] - x[0])**2))
+      rowAns = [*row[9:], (0,0)]
+      for i in range(1, len(row) - 7):
+        row[i] = row[i:i + 8]
+      row = row[1:-7]
+      result += row
+      ans += rowAns
+    arr = np.array(result)
+    out = np.array(ans)
     # Save the numpy array to a file
     print(f'Saving chunk {i} with shape {arr.shape}')
     print(f'Saving chunk {i} with shape {out.shape}')
